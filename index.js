f254857033c416a9a113c8069cf30971ae6737ec
minaa1111 <minagct@gmail.com>
2025-01-06 16:07:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dbConnect = require("./dbConnect");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const authRouter=require('./routes/authRoute')
+dbConnect();
+
+app.use('/',(req,res)=>{
+    res.send("Hello from server side");
+});
+
+app.use('/api/user',authRouter)
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
---
+++ routes/authRoute.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = expess.routerouter();
+
+router.post("/register");
+module.exports = router;
